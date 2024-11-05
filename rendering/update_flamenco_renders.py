5495754e1a541ff70ca59e5515c00cf9fb920199
Sam Van Hulle <sam@rombiezobots.com>
2024-11-05 15:09:51 +0100

    added tool to auto update all referenced Flamenco renders

---
+++ rendering/update_flamenco_renders.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import bpy
+import re
+
+print('=========================')
+print('UPDATING FLAMENCO RENDERS')
+print('=========================')
+
+# Regular expression to match Flamenco's folder naming.
+regex = re.compile(r'[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{6}')
+
+# Make all paths absolute.
+bpy.ops.file.make_paths_absolute()
+
+for img in bpy.data.images:
+
+    # Skip Render Result and Viewer Node, as well as image datablocks with an empty filepath.
+    if not img.filepath or img.filepath == '':
+        continue
+
+    # Get the latest date_time formatted folder in the image's grandparent folder.
+    parent_dir = Path(img.filepath).parent.parent
+    contents = sorted(parent_dir.iterdir(), reverse=True)
+    latest_path = next((dir for dir in contents if dir.is_dir() and regex.match(dir.stem)), None)
+    if not latest_path:
+        continue
+
+    # If the current image is not in the latest Flamenco folder, update the filepath.
+    name_current_folder = Path(img.filepath).parent.stem
+    name_latest_folder = latest_path.stem
+    if name_current_folder != name_latest_folder:
+        print('Updating', img.name, 'from', name_current_folder, 'to', name_latest_folder)
+        filename = Path(img.filepath).name
+        img.filepath = str(latest_path / filename)
