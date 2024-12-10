8151b525378a17095c5eefda7c91b5a2ff5c2a96
Sam Van Hulle <sam@rombiezobots.com>
2024-12-10 11:48:30 +0100

    updated reset_normals for current Blender versions; now also works on all objects if no selection

--- modeling/reset_normals.py
+++ modeling/reset_normals.py
@@ -1,15 +1,13 @@
 import bpy
 
-context = bpy.context
-mesh_objects = [
-    ob for ob in context.selected_objects if ob.type == 'MESH' and not (ob.data.library or ob.data.override_library)
-]
-if len(mesh_objects) == 0:
-    raise RuntimeError('This tool only works with a selection of mesh objects.')
+C = bpy.context
+if len(C.selected_objects) > 0:
+    objects = [ob for ob in C.selected_objects if ob.type == 'MESH' and not (ob.library or ob.override_library)]
+else:
+    objects = [ob for ob in bpy.data.objects if ob.type == 'MESH' and not (ob.library or ob.override_library)]
 
-for ob in mesh_objects:
+for ob in objects:
     for edge in ob.data.edges:
         edge.use_edge_sharp = False
-    ob.data.use_auto_smooth = False
-    bpy.context.view_layer.objects.active = ob
+    C.view_layer.objects.active = ob
     bpy.ops.mesh.customdata_custom_splitnormals_clear()
